f9b6217deea153a80a64d0fd4489f9b4830b7c75
julien082 <85090836+julien082@users.noreply.github.com>
2021-05-31 09:25:24 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -7,47 +7,14 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-  if (message.content === '!oui') {  
+  if (message.content === '!ak') {  
     message.channel.send(`https://prnt.sc/xtcole`)};
   if (message.content === '!fal') {  
-    message.channel.send(`https://prnt.sc/xtcp9q`);
-  }
-});
-
-client.on('message', message => {
-  if (message.content === '!ak') {  
-    message.channel.send(`https://prnt.sc/xtcole`);
-}
-});
-client.on('message', message => {
-  if (message.content === '!ak') {  
-    message.channel.send(`https://prnt.sc/xtcole`);
-}
-});
-client.on('message', message => {
-  if (message.content === '!ak') {  
-    message.channel.send(`https://prnt.sc/xtcole`);
-}
-});
-client.on('message', message => {
-  if (message.content === '!ak') {  
-    message.channel.send(`https://prnt.sc/xtcole`);
-}
-});
-client.on('message', message => {
-  if (message.content === '!ak') {  
-    message.channel.send(`https://prnt.sc/xtcole`);
-}
-});
-client.on('message', message => {
-  if (message.content === '!ak') {  
-    message.channel.send(`https://prnt.sc/xtcole`);
-}
-});
-client.on('message', message => {
-  if (message.content === '!ak') {  
-    message.channel.send(`https://prnt.sc/xtcole`);
-}
+    message.channel.send(`https://prnt.sc/xtcp9q`)};
+  if (message.content === '!rpk') {  
+    message.channel.send(`https://prnt.sc/xtcs45`)};
+  if (message.content === '!m1') {  
+    message.channel.send(`https://prnt.sc/xtcsym`)};  
 });
 
 client.login(process.env.TOKEN)
