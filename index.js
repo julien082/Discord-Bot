58aa3ec39c714a32604c13cd895c5c775ae60db1
julien082 <85090836+julien082@users.noreply.github.com>
2021-05-31 09:13:47 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -7,15 +7,13 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === '!ak') {  
-      message.channel.send(`https://prnt.sc/xtcole`);
-  }
-});
-client.on('message', message => {
   if (message.content === '!oui') {  
     message.channel.send(`https://prnt.sc/xtcole`);
-}
+  if (message.content === '!fal') {  
+    message.channel.send(`https://prnt.sc/xtcp9q`);
+  }
 });
+
 client.on('message', message => {
   if (message.content === '!ak') {  
     message.channel.send(`https://prnt.sc/xtcole`);
