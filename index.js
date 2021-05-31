d10727e98a32912b00d0aea605c2d0e1ce60fe69
julien082 <85090836+julien082@users.noreply.github.com>
2021-05-31 09:29:30 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -7,12 +7,32 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
+  if (message.content === '!map') {  
+    message.channel.send(`Interactive : https://www.gamemaps.co.uk/game/tarkov/maps/glory4life_updated_feb_2021
+
+    Factory : https://escapefromtarkov-fr.gamepedia.com/Usine
+    Customs : https://escapefromtarkov-fr.gamepedia.com/Douanes
+    Interchange : https://escapefromtarkov-fr.gamepedia.com/%C3%89changeur
+    Shoreline : https://escapefromtarkov-fr.gamepedia.com/Littoral
+    The Lab : https://escapefromtarkov-fr.gamepedia.com/Labo
+    Woods : https://escapefromtarkov-fr.gamepedia.com/Bois
+    Military : https://escapefromtarkov-fr.gamepedia.com/Base_militaire`)}; 
+  if (message.content === '!rpk') {  
+    message.channel.send(`https://prnt.sc/xtcs45`)}; 
+  if (message.content === '!rpk') {  
+    message.channel.send(`https://prnt.sc/xtcs45`)}; 
+  if (message.content === '!rpk') {  
+    message.channel.send(`https://prnt.sc/xtcs45`)}; 
+  if (message.content === '!rpk') { 
+    message.channel.send(`https://prnt.sc/xtcs45`)};   
+
+
   if (message.content === '!ak') {  
     message.channel.send(`https://prnt.sc/xtcole`)};
+  if (message.content === '!rpk') {  
+    message.channel.send(`https://prnt.sc/xtcs45`)}; 
   if (message.content === '!fal') {  
     message.channel.send(`https://prnt.sc/xtcp9q`)};
-  if (message.content === '!rpk') {  
-    message.channel.send(`https://prnt.sc/xtcs45`)};
   if (message.content === '!m1') {  
     message.channel.send(`https://prnt.sc/xtcsym`)};  
 });
