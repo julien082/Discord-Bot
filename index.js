81f32d14037a0b4999015799085d43492f5df723
julien082 <85090836+julien082@users.noreply.github.com>
2021-05-31 06:03:28 +0200

    premier essai

---
+++ index.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('message', message => {
+    if (message.content === '!ak') {  
+      message.channel.send(`https://prnt.sc/xtcole`);
+  }
+});
+client.on('message', message => {
+  if (message.content === '!oui') {  
+    message.channel.send(`https://prnt.sc/xtcole`);
+}
+});
+client.on('message', message => {
+  if (message.content === '!ak') {  
+    message.channel.send(`https://prnt.sc/xtcole`);
+}
+});
+client.on('message', message => {
+  if (message.content === '!ak') {  
+    message.channel.send(`https://prnt.sc/xtcole`);
+}
+});
+client.on('message', message => {
+  if (message.content === '!ak') {  
+    message.channel.send(`https://prnt.sc/xtcole`);
+}
+});
+client.on('message', message => {
+  if (message.content === '!ak') {  
+    message.channel.send(`https://prnt.sc/xtcole`);
+}
+});
+client.on('message', message => {
+  if (message.content === '!ak') {  
+    message.channel.send(`https://prnt.sc/xtcole`);
+}
+});
+client.on('message', message => {
+  if (message.content === '!ak') {  
+    message.channel.send(`https://prnt.sc/xtcole`);
+}
+});
+client.on('message', message => {
+  if (message.content === '!ak') {  
+    message.channel.send(`https://prnt.sc/xtcole`);
+}
+});
+
+client.login(process.env.TOKEN)
