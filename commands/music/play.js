9b4725837435a9b2a539d16b782994c99a1c37dc
julien082 <85090836+julien082@users.noreply.github.com>
2021-06-03 03:58:59 +0200

    Add files via upload

--- commands/music/play.js
+++ commands/music/play.js
@@ -6,6 +6,7 @@ module.exports = class PlayCommand extends Command {
     constructor(client) {
         super(client, {
             name: 'lire',
+            aliases: ['l'],
             group: 'music',
             memberName: 'lire',
             description: 'Lit une musique Youtube',
