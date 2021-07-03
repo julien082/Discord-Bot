35fae1afa5800a08db91884ee38c4b0a308483c8
julien082 <85090836+julien082@users.noreply.github.com>
2021-07-04 00:53:33 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ client.on('message', message => {
   if (message.content === '!help') {  
     message.channel.send(`● Comandes: 
 !clear (supprimer messages)
-'''css● Tarkov''' : !map, !quest, !ammo, !key, !ledx, !graphic, !scav.
+● Tarkov : !map, !quest, !ammo, !key, !ledx, !graphic, !scav.
 ● Armes : 
 - Assauts : !m4, !hk, !rpk, !ak, !akm, !akms, !ak101, !ak103, !vepr, !dt, !val.
 - Mitraillettes : !mpx, !mp5, !mp7, !p90, !vector.
