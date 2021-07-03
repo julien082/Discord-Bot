60dfc6c6ea7e808844012c898f0fc93032b115d3
julien082 <85090836+julien082@users.noreply.github.com>
2021-07-04 01:21:17 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { downloadFromInfo } = require('ytdl-core');
 const client = new Discord.Client();
 const ytdl = require("ytdl-core");
 const queue = new Map();
@@ -27,9 +28,11 @@ client.once('ready', () => {
 /** Commandes */
 client.on('message', message => {
   if (message.content === '!help') {  
-    message.channel.send(`● `**Comandes**`: 
+    message.channel.send(`● **Comandes**: 
 !clear (supprimer messages)
-● `**Tarkov**` : !map, !quest, !ammo, !key, !ledx, !graphic, !scav.
+● ```diff
+Tarkov
+``` : !map, !quest, !ammo, !key, !ledx, !graphic, !scav.
 ● **Armes** : 
 - **Assauts** : !m4, !hk, !rpk, !ak, !akm, !akms, !ak101, !ak103, !vepr, !dt, !val.
 - **Mitraillettes** : !mpx, !mp5, !mp7, !p90, !vector.
