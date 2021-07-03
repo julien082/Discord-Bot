44549281cb6f3d7d308f26f3bc781694918d1b3b
julien082 <85090836+julien082@users.noreply.github.com>
2021-07-04 01:49:22 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ client.once('ready', () => {
 /** Commandes */
 client.on('message', message => {
   if (message.content === '!help') {  
-    message.channel.send(`>>> ● **Comandes** : !clear (supprimer messages)
-● [{"yellow":Tarkov}] : !map, !quest, !ammo, !key, !ledx, !graphic, !scav.
+    message.channel.send(`>>> ● :bitcoin:**Comandes** : !clear (supprimer messages).
+● **Tarkov** : !map, !quest, !ammo, !key, !ledx, !graphic, !scav.
 ● **Armes** : 
 - **Assauts** : !m4, !hk, !rpk, !ak, !akm, !akms, !ak101, !ak103, !vepr, !dt, !val.
 - **Mitraillettes** : !mpx, !mp5, !mp7, !p90, !vector.
