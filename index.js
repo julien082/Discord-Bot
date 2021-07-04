8b463104d0df4d46e47010df7eb56a4633f87dc0
julien082 <85090836+julien082@users.noreply.github.com>
2021-07-04 08:28:42 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -140,12 +140,16 @@ if (message.content === '!testt') {
   const embed = new Discord.MessageEmbed()
   .setColor('#9EA440')
   .setTitle('<a:commandes:861037555301220413>  **Commandes :** ')
-  .addFields(
-    { name: '!clear (supprimer les messages)', inline: false },
-    { name: '<:tarkov:861045088900743228>  **Tarkov**', value: '!map, !quest, !ammo, !key, !ledx, !graphic, !scav.', inline: false },
-    { name: '<:assaut:861054281276719125>  **Assauts** :' , value: '!m4, !hk, !rpk, !ak, !akm, !akms, !ak101, !ak103, !vepr, !dt, !val.', inline: false }
-  )
   .setThumbnail('https://prnt.sc/18fi01h')
+  .addFields(
+    { name: '!clear (supprimer les messages)', value:'', inline: false },
+    { name: '<:tarkov:861045088900743228>  **Tarkov**', value: '!map, !quest, !ammo, !key, !ledx, !graphic, !scav.', inline: false },
+    { name: '<:assaut:861054281276719125>  **Assauts** :' , value: '!m4, !hk, !rpk, !ak, !akm, !akms, !ak101, !ak103, !vepr, !dt, !val.', inline: false },
+    { name: '<:weapon:861047801239830539>  **Mitraillettes**', value: '!mpx, !mp5, !mp7, !p90, !vector.', inline: false },
+    { name: '<:rifle:861050143813926943>  **Coup par coup**', value: '!adar, !fal, !sr, !tx.', inline: false },
+    { name: '<:sniper:861047828607270922>  **Snipes**', value: '!sks, !svds, !m1, !m700, !t5000, !rsass, !mk.', inline: false },
+  )
+  
   
   return message.channel.send(embed)
   }
