29e7d5de9bcfa826f9687dc8871c5aef146f4559
julien082 <85090836+julien082@users.noreply.github.com>
2021-07-04 10:45:21 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ if (message.content === '!testt') {
   .addFields(
     { name: '!clear ', value: '(supprimer messages)', inline: true },
     { name: '<:tarkov:861045088900743228>  **Tarkov** :', value: '!map, !quest, !ammo, !key, !ledx, !graphic, !scav.', inline: false },
-    { name: '● **Armes**', value: 'oui', inline: false},
+    { name: '● **Armes**', value: 'oui \n\u200b', inline: false},
     { name: '<:assaut:861054281276719125>  **Assauts** :' , value: '!m4, !hk, !rpk, !ak, !akm, !akms, !ak101, !ak103, !vepr, !dt, !val.', inline: false },
     { name: '<:weapon:861047801239830539>  **Mitraillettes** :', value: '!mpx, !mp5, !mp7, !p90, !vector.', inline: false },
     { name: '<:rifle:861050143813926943>  **Coup par coup** :', value: '!adar, !fal, !sr, !tx.', inline: false },
