e0f1ea93df4bc1969e6eec3be30121bb975b9c17
julien082 <85090836+julien082@users.noreply.github.com>
2021-07-04 09:59:24 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -138,12 +138,12 @@ if (message.content === '!testt') {
   - <:sniper:861047828607270922>  **Snipes** : !sks, !svds, !m1, !m700, !t5000, !rsass, !mk.`);
 
   const {MessageAttachment} = require ('discord.js')
-  const diceImg = new MessageAttachment ('./Images/Killaa.jpg')
+  const diceImg = new MessageAttachment ('./Images/killaa.jpg')
   const embed = new Discord.MessageEmbed()
   .setColor("#9EA440")
   .setTitle('<a:commandes:861037555301220413>  **Commandes :** ')
   .attachFiles(diceImg)
-  .setThumbnail('attachment://Killaa.jpg')
+  .setThumbnail('attachment://killaa.jpg')
   .addFields(
     { name: '!clear ', value:'(supprimer les messages)', inline: false },
     { name: '<:tarkov:861045088900743228>  **Tarkov** :', value: '!map, !quest, !ammo, !key, !ledx, !graphic, !scav.', inline: false },
