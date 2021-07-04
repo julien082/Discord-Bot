1cdeb76f874c4d012f834fef7c6703f55ea7801b
julien082 <85090836+julien082@users.noreply.github.com>
2021-07-04 04:34:13 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -34,13 +34,13 @@ client.on('message', message => {
    
 ● **Armes** : 
    
-- <:assaut:861054281276719125>   **Assauts** : !m4, !hk, !rpk, !ak, !akm, !akms, !ak101, !ak103, !vepr, !dt, !val.
+- <:assaut:861054281276719125>  **Assauts** : !m4, !hk, !rpk, !ak, !akm, !akms, !ak101, !ak103, !vepr, !dt, !val.
    
-- <:weapon:861047801239830539>   **Mitraillettes** : !mpx, !mp5, !mp7, !p90, !vector.
+- <:weapon:861047801239830539>  **Mitraillettes** : !mpx, !mp5, !mp7, !p90, !vector.
    
-- <:rifle:861050143813926943>   **Coup par coup** : !adar, !fal, !sr, !tx.
+- <:rifle:861050143813926943>  **Coup par coup** : !adar, !fal, !sr, !tx.
    
-- <:sniper:861047828607270922>   **Snipes** : !sks, !svds, !m1, !m700, !t5000, !rsass, !mk.`)};
+- <:sniper:861047828607270922>  **Snipes** : !sks, !svds, !m1, !m700, !t5000, !rsass, !mk.`)};
   if (message.content === '!map') {  
     message.channel.send(`Interactive : <https://www.gamemaps.co.uk/game/tarkov/maps/glory4life_updated_feb_2021>
 
