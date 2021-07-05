37ee6be1657e173e7ef150b2211e355ff082cfc3
julien082 <85090836+julien082@users.noreply.github.com>
2021-07-05 03:48:19 +0200

    Add files via upload

---
+++ commandes/help.js
@@ -0,0 +1,26 @@
+const {  MessageEmbed } = require('discord.js')
+const {MessageAttachment} = require ('discord.js')
+const diceImg = new MessageAttachment ('./Images/kikiki.png')
+
+module.exports = {
+    name: 'help',
+    description: 'commandes bot',
+    execute(message, args){
+      const embed = new MessageEmbed()  
+          .setColor("#FDFEFE")
+          .setTitle('<a:commandes:861037555301220413>  **Commandes :**')
+          .attachFiles(diceImg)
+          .setImage('attachment://kikiki.png')
+          .addFields(
+            { name: '\u200b**!clear** (supprimer messages)', value: '\u200b', inline: true },
+            { name: '<:tarkov:861045088900743228>  **Tarkov** :', value: '!map, !quest, !ammo, !key, !ledx, !graphic, !scav.', inline: false },
+            { name: '\u200b', value:'● **Armes** :', inline: false},
+            { name: '<:assaut:861054281276719125>  **Assauts** :' , value: '  !m4, !hk, !rpk, !ak, !akm, !akms, !ak101, !ak103, !vepr, !dt, !val.', inline: false },
+            { name: '<:weapon:861047801239830539>  **Mitraillettes** :', value: '  !mpx, !mp5, !mp7, !p90, !vector.', inline: false },
+            { name: '<:rifle:861050143813926943>  **Coup par coup** :', value: '  !adar, !fal, !sr, !tx.', inline: false },
+            { name: '<:sniper:861047828607270922>  **Snipes** :', value: '  !sks, !svds, !m1, !m700, !t5000, !rsass, !mk.', inline: false },
+          )
+          message.channel.send(embed)
+          }
+        }
+    
--- commandes/tarkov.js
+++ commandes/tarkov.js
@@ -1,139 +1,95 @@
-module.exports = {
-    execute(message){  
+module.exports = async function (message){ 
       if (message.content === '!key') {  
         message.channel.send(`https://prnt.sc/r9dlgs https://prnt.sc/10o12ur`)}; 
       if (message.content === '!map') {  
         message.channel.send(`Factory : <https://escapefromtarkov-fr.gamepedia.com/Usine>
-        Customs : <https://escapefromtarkov-fr.gamepedia.com/Douanes>
-        Interchange : <https://escapefromtarkov-fr.gamepedia.com/%C3%89changeur>
-        Shoreline : <https://escapefromtarkov-fr.gamepedia.com/Littoral>
-        The Lab : <https://escapefromtarkov-fr.gamepedia.com/Labo>
-        Woods : <https://escapefromtarkov-fr.gamepedia.com/Bois>
-        Military : <https://escapefromtarkov-fr.gamepedia.com/Base_militaire>`)}; 
-        
-        /**
-`)
-    },
-    name: 'quest',
-    description: 'commandes quetes',
-    execute(message){
-      message.channel.send('https://media.discordapp.net/attachments/776801862778093568/798907782635716658/Objets_de_quetes.jpg?width=1346&height=676')
-    },
-  }
-  /**
-    name: 'ammo',
-    description: 'commandes quetes',
-    execute(message){
-      message.channel.send('https://tarkov.ascheron.dev')
-    },
-    name: 'key',
-    description: 'commandes quetes',
-    execute(message){
-      message.channel.send('https://prnt.sc/r9dlgs https://prnt.sc/10o12ur')
-    },
-    name: 'ledx',
-    description: 'commandes quetes',
-    execute(message){
-      message.channel.send('104 Ouest | 205 Ouest | 301 Ouest | 107 Est | 222-226 Est | 313-314 Est | Emercom | Blue Keycard | Green Keycard')
-    },
-    name: 'graphic',
-    description: 'commandes quetes',
-    execute(message){
-      message.channel.send('https://escapefromtarkov.fandom.com/fr/wiki/Fichier:BTC_time_to_graphics_card_chart.png')  
-    },
-    name: 'scav',
-    description: 'commandes quetes',
-    execute(message){
-      message.channel.send(`https://cdn.discordapp.com/attachments/776801862778093568/860476889650561034/EFT_Scav_Code_FR.png
-    Update: 
-    ● La réputation chez Fence ne diminuera désormais pas si le joueur a tué un Scav qui a attaqué en premier.`)
-    },
-  }
-*/
-        /**
-          if (message.content === '!quest') {  
+      Customs : <https://escapefromtarkov-fr.gamepedia.com/Douanes>
+      Interchange : <https://escapefromtarkov-fr.gamepedia.com/%C3%89changeur>
+      Shoreline : <https://escapefromtarkov-fr.gamepedia.com/Littoral>
+      The Lab : <https://escapefromtarkov-fr.gamepedia.com/Labo>
+      Woods : <https://escapefromtarkov-fr.gamepedia.com/Bois>
+      Military : <https://escapefromtarkov-fr.gamepedia.com/Base_militaire>`)}; 
+      if (message.content === '!quest') {  
             message.channel.send(`https://media.discordapp.net/attachments/776801862778093568/798907782635716658/Objets_de_quetes.jpg?width=1346&height=676`)}; 
-          if (message.content === '!ammo') {  
+      if (message.content === '!ammo') {  
             message.channel.send(`https://tarkov.ascheron.dev/`)}; 
-          if (message.content === '!key') {  
+      if (message.content === '!key') {  
             message.channel.send(`https://prnt.sc/r9dlgs https://prnt.sc/10o12ur`)}; 
-           if (message.content === '!ledx') {  
+      if (message.content === '!ledx') {  
             message.channel.send(`104 Ouest | 205 Ouest | 301 Ouest | 107 Est | 222-226 Est | 313-314 Est | Emercom | Blue Keycard | Green Keycard`)};
-          if (message.content === '!graphic') { 
+      if (message.content === '!graphic') { 
             message.channel.send(`https://escapefromtarkov.fandom.com/fr/wiki/Fichier:BTC_time_to_graphics_card_chart.png`)};   
-          if (message.content === '!scav') { 
+      if (message.content === '!scav') { 
             message.channel.send(`https://cdn.discordapp.com/attachments/776801862778093568/860476889650561034/EFT_Scav_Code_FR.png
-        Update:
-        ● La réputation chez Fence ne diminuera désormais pas si le joueur a tué un Scav qui a attaqué en premier.`)};   
+      Update:
+      ● La réputation chez Fence ne diminuera désormais pas si le joueur a tué un Scav qui a attaqué en premier.`)};   
         
-          if (message.content === '!m4') {  
+      if (message.content === '!m4') {  
             message.channel.send(`https://prnt.sc/xtckq4 https://prnt.sc/xtclh5`)};
-          if (message.content === '!hk') {  
+      if (message.content === '!hk') {  
             message.channel.send(`https://prnt.sc/xtcqqu`)};
-          if (message.content === '!ak') {  
+      if (message.content === '!ak') {  
             message.channel.send(`https://prnt.sc/xtcole`)};
-          if (message.content === '!akm') {  
+      if (message.content === '!akm') {  
             message.channel.send(`https://prnt.sc/xtcnyk`)};
-          if (message.content === '!akms') {  
+      if (message.content === '!akms') {  
             message.channel.send(`https://prnt.sc/xtd5f2`)};
-          if (message.content === '!ak101') {  
+      if (message.content === '!ak101') {  
             message.channel.send(`https://prnt.sc/xtcmwd`)};
-          if (message.content === '!ak103') {  
+      if (message.content === '!ak103') {  
             message.channel.send(`https://prnt.sc/13p32ym`)};
-          if (message.content === '!rpk') {  
+      if (message.content === '!rpk') {  
             message.channel.send(`https://prnt.sc/xtcs45`)};
-          if (message.content === '!vepr') {  
+      if (message.content === '!vepr') {  
             message.channel.send(`https://prnt.sc/q78tlr`)};
-          if (message.content === '!dt') {  
+      if (message.content === '!dt') {  
             message.channel.send(`https://prnt.sc/xtd2ga`)};
-          if (message.content === '!val') {  
+      if (message.content === '!val') {  
             message.channel.send(`https://prnt.sc/vgwi3l`)};
         
-          if (message.content === '!mp5') {  
+      if (message.content === '!mp5') {  
             message.channel.send(`https://prnt.sc/xtd34d`)};
-          if (message.content === '!mp7') {  
+      if (message.content === '!mp7') {  
             message.channel.send(`https://prnt.sc/xtd18r`)};
-          if (message.content === '!mpx') {  
+      if (message.content === '!mpx') {  
             message.channel.send(`https://prnt.sc/xtd6b8`)};
-          if (message.content === '!p90') {  
+      if (message.content === '!p90') {  
             message.channel.send(`https://prnt.sc/xtcole`)};
-          if (message.content === '!vector') {  
+      if (message.content === '!vector') {  
             message.channel.send(`https://prnt.sc/xtcole`)};
         
-          if (message.content === '!adar') {  
+      if (message.content === '!adar') {  
             message.channel.send(`http://prntscr.com/q4c3t6`)};
-          if (message.content === '!fal') {  
+      if (message.content === '!fal') {  
             message.channel.send(`https://prnt.sc/xtcp9q`)};
-          if (message.content === '!sr') {  
+      if (message.content === '!sr') {  
             message.channel.send(`https://prnt.sc/xtcy5r`)};
-          if (message.content === '!tx') {  
+      if (message.content === '!tx') {  
             message.channel.send(`https://prnt.sc/xtcj6b`)};
         
         
-          if (message.content === '!sks') {  
+      if (message.content === '!sks') {  
             message.channel.send(`https://prnt.sc/xtcfd5`)};
-          if (message.content === '!svds') {  
+      if (message.content === '!svds') {  
             message.channel.send(`https://prnt.sc/xtcole`)};
-          if (message.content === '!m1') {  
+      if (message.content === '!m1') {  
             message.channel.send(`https://prnt.sc/xtcsym`)};
-          if (message.content === '!m700') {  
+      if (message.content === '!m700') {  
             message.channel.send(`https://prnt.sc/xtcrdm`)};
-          if (message.content === '!t5000') {  
+      if (message.content === '!t5000') {  
             message.channel.send(`https://prnt.sc/t1thcp`)};
-          if (message.content === '!rsass') {  
+      if (message.content === '!rsass') {  
             message.channel.send(`https://prnt.sc/xtcw7j`)};
-          if (message.content === '!mk') {  
+      if (message.content === '!mk') {  
             message.channel.send(`https://prnt.sc/17idxmr`)};  
         
-          if (message.content === '!dododex') {  
+      if (message.content === '!dododex') {  
             message.channel.send(`https://www.dododex.com/`)}; 
         
-          if (message.content === '!breton') {  
+      if (message.content === '!breton') {  
             message.channel.send(`https://tenor.com/view/linksthesun-breton-bretagne-galettes-gif-7912654`)}; 
-          if (message.content === '!chauve') {  
+      if (message.content === '!chauve') {  
             message.channel.send(`https://tenor.com/view/calou-eggs-head-bald-gif-16129954`)}; 
-            if (message.content === '!test') {  
-              message.channel.send('')}; 
-              */
-
-            }}
+      if (message.content === '!test') {  
+              message.channel.send('')};
+        }
