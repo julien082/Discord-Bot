f22fa3bc0c49de400cc4b6a5d748456f30419538
julien082 <85090836+julien082@users.noreply.github.com>
2021-12-28 05:22:53 +0100

    Add files via upload

---
+++ commandes/Escape From Tarkov.js
@@ -0,0 +1,110 @@
+module.exports = async function (message) { 
+      
+      if (message.content === '!film') {  
+            message.channel.send(`https://www.youtube.com/watch?v=q9OIT7W24V8&t=1062s`)}; 
+      if (message.content === '!patch') {  
+            message.channel.send(`https://www.escapefromtarkov.com/news/page/0`)}; 
+      if (message.content === '!key') {  
+        message.channel.send(`https://prnt.sc/r9dlgs https://prnt.sc/10o12ur`)}; 
+      if (message.content === '!map') {  
+        message.channel.send(`Factory : <https://escapefromtarkov-fr.gamepedia.com/Usine>
+Customs : <https://escapefromtarkov-fr.gamepedia.com/Douanes>
+Interchange : <https://escapefromtarkov-fr.gamepedia.com/%C3%89changeur>
+Shoreline : <https://escapefromtarkov-fr.gamepedia.com/Littoral>
+The Lab : <https://escapefromtarkov-fr.gamepedia.com/Labo>
+Woods : <https://escapefromtarkov-fr.gamepedia.com/Bois>
+Military : <https://escapefromtarkov-fr.gamepedia.com/Base_militaire>
+Lighthouse : <https://escapefromtarkov.fandom.com/fr/wiki/Phare>
+==> En anglais en attendant: <https://escapefromtarkov.fandom.com/wiki/Lighthouse>`)}; 
+      if (message.content === '!quest') {  
+            message.channel.send(`https://media.discordapp.net/attachments/776801862778093568/798907782635716658/Objets_de_quetes.jpg?width=1346&height=676`)}; 
+      if (message.content === '!ammo') {  
+            message.channel.send(`https://tarkov.ascheron.dev/`)}; 
+      if (message.content === '!key') {  
+            message.channel.send(`https://prnt.sc/r9dlgs https://prnt.sc/10o12ur`)}; 
+      if (message.content === '!ledx') {  
+            message.channel.send(`104 Ouest | 205 Ouest | 301 Ouest | 107 Est | 222-226 Est | 313-314 Est | Emercom | Blue Keycard | Green Keycard`)};
+      if (message.content === '!graphic') { 
+            message.channel.send(`https://escapefromtarkov.fandom.com/fr/wiki/Fichier:BTC_time_to_graphics_card_chart.png`)};   
+      if (message.content === '!scav') { 
+            message.channel.send(`https://cdn.discordapp.com/attachments/776801862778093568/862285941620342835/EFT_Scav_Code_FR.png
+Update:
+● La réputation chez Fence ne diminuera désormais pas si le joueur a tué un Scav qui a attaqué en premier.`)};   
+      if (message.content === '!modding') {  
+            message.channel.send(`https://bit.ly/3f9b8Ik`)};
+      if (message.content === '!postfx') { 
+            message.channel.send(`[furl3x's PostFX]: -Brightness: 100 -Saturation: 20 -Clarity: 100 -Colorfulness: 20 -Luma Sharpen: 100 -Adaptive Sharpen: 20 -Boost (Color Grading): 100`)};
+                  
+                  
+      if (message.content === '!m4') {  
+            message.channel.send(`https://prnt.sc/1gptd1y ~280 000₽`)};
+      if (message.content === '!hk') {  
+            message.channel.send(`https://prnt.sc/1gptr9r ~250 000₽`)};
+      if (message.content === '!mk') {  
+            message.channel.send(`https://prnt.sc/1gprb7r ~300 000₽ https://prnt.sc/1h3u6lj ~200 000₽`)};
+      if (message.content === '!ak') {  
+            message.channel.send(`https://prnt.sc/1gpppzw ~170 000₽`)};
+      if (message.content === '!akm') {  
+            message.channel.send(`https://prnt.sc/xtcnyk`)};
+      if (message.content === '!akms') {  
+            message.channel.send(`https://prnt.sc/xtd5f2`)};
+      if (message.content === '!ak101') {  
+            message.channel.send(`https://prnt.sc/xtcmwd`)};
+      if (message.content === '!ak103') {  
+            message.channel.send(`https://prnt.sc/13p32ym`)};
+      if (message.content === '!rpk') {  
+            message.channel.send(`https://prnt.sc/1gptxj0 ~140 000₽`)};
+      if (message.content === '!vepr') {  
+            message.channel.send(`https://prnt.sc/q78tlr`)};
+      if (message.content === '!dt') {  
+            message.channel.send(`https://prnt.sc/xtd2ga`)};
+      if (message.content === '!val') {  
+            message.channel.send(`https://prnt.sc/vgwi3l`)};
+        
+      if (message.content === '!mp5') {  
+            message.channel.send(`https://prnt.sc/1gpsirf ~120 000₽`)};
+      if (message.content === '!mp7') {  
+            message.channel.send(`https://prnt.sc/xtd18r`)};
+      if (message.content === '!mpx') {  
+            message.channel.send(`https://prnt.sc/1gpsrgi ~160 000₽`)};
+      if (message.content === '!p90') {  
+            message.channel.send(`https://prnt.sc/1rjuua5`)};
+      if (message.content === '!vector') {  
+            message.channel.send(`https://prnt.sc/1rju7s5`)};
+        
+      if (message.content === '!adar') {  
+            message.channel.send(`http://prntscr.com/q4c3t6`)};
+      if (message.content === '!fal') {  
+            message.channel.send(`https://prnt.sc/xtcp9q`)};
+      if (message.content === '!sr') {  
+            message.channel.send(`https://prnt.sc/1i07j56 ~250 000₽`)};
+      if (message.content === '!tx') {  
+            message.channel.send(`https://prnt.sc/1gpqw7e ~300 000₽`)};
+        
+        
+      if (message.content === '!sks') {  
+            message.channel.send(`https://prnt.sc/xtcfd5`)};
+      if (message.content === '!svds') {  
+            message.channel.send(`https://prnt.sc/1rjuf2r`)};
+      if (message.content === '!m1a') {  
+            message.channel.send(`https://prnt.sc/1gppvwg ~270 000₽`)};
+      if (message.content === '!m700') {  
+            message.channel.send(`https://prnt.sc/1gpt6iu ~180 000₽`)};
+      if (message.content === '!t5000') {  
+            message.channel.send(`https://prnt.sc/t1thcp`)};
+      if (message.content === '!rsass') {  
+            message.channel.send(`https://prnt.sc/xtcw7j`)};
+      if (message.content === '!mk18') {  
+            message.channel.send(`https://prnt.sc/17idxmr`)}; 
+        
+      if (message.content === '!dododex') {  
+            message.channel.send(`https://www.dododex.com/`)}; 
+        
+      if (message.content === '!breton') {  
+            message.channel.send(`https://tenor.com/view/linksthesun-breton-bretagne-galettes-gif-7912654`)}; 
+      if (message.content === '!chauve') {  
+            message.channel.send(`https://tenor.com/view/calou-eggs-head-bald-gif-16129954`)}; 
+      if (message.content === '!test') {  
+              message.channel.send('')
+      };
+    }
