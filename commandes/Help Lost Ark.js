124c60402a80c398463f2bddaffe456a22eb5fa6
julien082 <85090836+julien082@users.noreply.github.com>
2022-01-15 02:49:55 +0100

    Add files via upload

--- commandes/Help Lost Ark.js
+++ commandes/Help Lost Ark.js
@@ -14,7 +14,7 @@ module.exports = {
           .setThumbnail(message.author.avatarURL({ dynamic: true, size: 256 }))
           .addFields(          
             { name: '\u200b', value: '**!clear**᲼( supprimer messages )᲼᲼᲼᲼᲼᲼\u200b᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼', inline: false },
-            { name: '<:lostark:931723069599395880>  **Lost Ark** :', value: '!berserker, !paladin, !pistolancier, !essentialiste, !elementaliste, !pugiliste, !spirite, !fusiliere, !artilleur, !franctireur, !salve, !barde, !sorciere, !demoniste, !sanguelame', inline: false },
+            { name: '<:lostark:931723577269571773>  **Lost Ark** :', value: '!berserker, !paladin, !pistolancier, !essentialiste, !elementaliste, !pugiliste, !spirite, !fusiliere, !artilleur, !franctireur, !salve, !barde, !sorciere, !demoniste, !sanguelame', inline: false },
           )
           message.channel.send(embed)
           }
