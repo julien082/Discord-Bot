5f1a7b72c281084920e635f9e55e2ed415cc41e7
julien082 <85090836+julien082@users.noreply.github.com>
2022-01-15 03:19:47 +0100

    Add files via upload

---
+++ Help Lost Ark.js
@@ -0,0 +1,27 @@
+const {  MessageEmbed } = require('discord.js')
+const {MessageAttachment} = require ('discord.js')
+const diceImg = new MessageAttachment ('./Images/km.png')
+
+module.exports = {
+    name: 'lostark',
+    description: 'commandes bot',
+    execute( message, args ){
+      const embed = new MessageEmbed()  
+          .setColor('#FDFEFE')
+          .setTitle('<a:commandes:861037555301220413>᲼**Commandes :**')
+          .attachFiles(diceImg)
+          .setImage('attachment://km.png')
+          .setThumbnail(message.author.avatarURL({ dynamic: true, size: 256 }))
+          .addFields(          
+            { name: '\u200b', value: '**!clear**᲼( supprimer messages )᲼᲼᲼᲼᲼᲼\u200b᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼', inline: false },
+            { name: '<:lostark:931723577269571773>   **Lost Ark** :', value: '!map', inline: false },
+            { name: '\u200b', value:'● **Classes** :', inline: false},   
+            { name: '<:guerriers:931734212980138025>  **Guerriers** :' , value: '  !berserker, !paladin, !pistolancier.', inline: false },
+            { name: '<:weapon:861047801239830539>  **Martialistes** :', value: '  !essentialiste, !elementaliste, !pugiliste, !spirite.', inline: false },
+            { name: '<:rifle:861050143813926943>   **Tireurs** :', value: '  !fusiliere, !artilleur, !franctireur, !salve.', inline: false },
+            { name: '<:sniper:861047828607270922>  **Mages** :', value: '  !barde, !sorciere.', inline: false },
+            { name: '<:assassins:931731189696454727>  **Assassins** :', value: '  !demoniste, !sanguelame.', inline: false }
+            )
+          message.channel.send(embed)
+          }
+        }
