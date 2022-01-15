a1b639c651942a191cae08753910434c7e7fc683
julien082 <85090836+julien082@users.noreply.github.com>
2022-01-15 21:23:19 +0100

    Add files via upload

--- commandes/Lost Ark.js
+++ commandes/Lost Ark.js
@@ -1,5 +1,23 @@
 module.exports = async function (message) { 
       
-    if (message.content === '!lostlml') {  
-          message.channel.send(`pouette`)}; 
+      
+      
+      /** CLasses */
+
+      /** Guerriers */
+      if (message.content === '!berserker') {  
+            message.channel.send(`https://www.youtube.com/watch?v=hZ0AJdAdOSc`)}; 
+      if (message.content === '!paladin') {  
+            message.channel.send(`https://www.youtube.com/watch?v=xUnabf4SqG4`)}; 
+      if (message.content === '!pistolancier') {  
+            message.channel.send(`https://www.youtube.com/watch?v=zMHewpt6DuM`)}; 
+      
+      /**Martialistes */
+      if (message.content === '!berserker') {  
+            message.channel.send(`https://www.youtube.com/watch?v=hZ0AJdAdOSc`)}; 
+      /** Tireurs */
+
+      /** Mages */
+
+      /** Assassins */
     }
