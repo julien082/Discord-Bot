fdc9640ffd561b71cd11e1cba617d39123ccad7e
julien082 <85090836+julien082@users.noreply.github.com>
2022-03-01 01:15:21 +0100

    Add files via upload

---
+++ Lost Ark.js
@@ -0,0 +1,99 @@
+module.exports = async function (message) { 
+       
+      if (message.content === '!site') {  
+            message.channel.send(`
+<https://papunika.com/>
+<https://lost-ark.maxroll.gg/>`)}; 
+      if (message.content === '!marchand') {  
+            message.channel.send(`
+**Tous ces marchands viennent à ces horaires pour une durée de 25 minutes**:
+
+**Lucas** (Yudia), **Mac** (Anich), **Jeffrey** (Silensierra), **Morris** (Luttéran Orriental) and **Dorella** (Feiton).
+- Horaires: 1:30, 4:30, 5:30, 7:30, 8:30, 11:30 (AM and PM).
+
+**Malone** (Luteran Occidental), **Burt** (Luteran Orriental), **Oliver** (Tortoyk), **Nox** (Artheline), **Aricer** (Rohendel) and **Rayni** (Punika).
+- Horaires: 2:30, 5:30, 6:30, 8:30, 9:30, 12:30 (AM and PM).
+
+**Ben** (Rethramis), **Peter** (Bern-Nord) and **Laitir** (Yorn).
+- Horaires: 3:30, 4:30, 6:30, 7:30, 10:30, 12:30 (AM and PM).
+            
+**Localisations**: 
+            
+**Burt**: <https://lostarkcodex.com/us/npc/80003/>
+**Morris**: <https://lostarkcodex.com/fr/npc/80018/>`)}; 
+
+      if (message.content === '!guide') {  
+            message.channel.send(`https://lostarkonline.fr/guides/`)}; 
+      if (message.content === '!build') {  
+            message.channel.send(`
+**Builds FR** : <https://tool.lostarkonline.fr/builds>
+**Builds Monde** : <https://lostarkcodex.com/us/skillbuilds/>
+**Calculateur De Gemmes** : <https://tool.lostarkonline.fr/gem>`)}; 
+
+      if (message.content === '!donjon') {  
+            message.channel.send(`https://lostarkonline.fr/donjon-des-abysses/`)}; 
+      if (message.content === '!raid') {  
+            message.channel.send(`https://lostarkonline.fr/raid-des-abysses/`)}; 
+      if (message.content === '!gardien') {  
+            message.channel.send(`https://lostarkonline.fr/raid-de-gardiens/`)}; 
+      if (message.content === '!raidhl') {  
+            message.channel.send(`https://lostarkonline.fr/raid-des-legions/`)}; 
+
+      
+
+      /** CLasses */
+      if (message.content === '!gameplay') {  
+            message.channel.send(`https://www.youtube.com/watch?v=D2VWzVI6PQI&list=PL9qPSTvySyd4CstpXgN8oIS1lerP2F2h2&index=1`)}; 
+
+      /** Guerriers */
+      if (message.content === '!berserker') {  
+            message.channel.send(`https://www.youtube.com/watch?v=hZ0AJdAdOSc`)}; 
+      if (message.content === '!paladin') {  
+            message.channel.send(`https://www.youtube.com/watch?v=xUnabf4SqG4`)}; 
+      if (message.content === '!pistolancier') {  
+            message.channel.send(`https://www.youtube.com/watch?v=zMHewpt6DuM`)}; 
+      
+      /**Martialistes */
+      if (message.content === '!essentialiste') {  
+            message.channel.send(`https://www.youtube.com/watch?v=TZfZBxYG71c`)};
+      if (message.content === '!elementiste') {  
+            message.channel.send(`https://www.youtube.com/watch?v=I4DZpv82sFw`)}; 
+      if (message.content === '!pugiliste') {  
+            message.channel.send(`https://www.youtube.com/watch?v=yiyIMmgh1mI`)}; 
+      if (message.content === '!spirite') {  
+            message.channel.send(`https://www.youtube.com/watch?v=jhV2bsy5_U0`)};  
+      /** Tireurs */
+      if (message.content === '!fusiliere') {  
+            message.channel.send(`https://www.youtube.com/watch?v=R5iQ32XxRKo`)};
+      if (message.content === '!artilleur') {  
+            message.channel.send(`https://www.youtube.com/watch?v=VkGSg9O-stc`)}; 
+      if (message.content === '!franctireur') {  
+            message.channel.send(`https://www.youtube.com/watch?v=EesjAG3gpd8`)}; 
+      if (message.content === '!salve') {  
+            message.channel.send(`https://www.youtube.com/watch?v=ZSO0V_OIUPQ&t=3s`)};  
+      /** Mages */
+      if (message.content === '!barde') {  
+            message.channel.send(`https://www.youtube.com/watch?v=SgpU3C_GdjY`)};
+      if (message.content === '!sorciere') {  
+            message.channel.send(`https://www.youtube.com/watch?v=apaNS4MDf18`)}; 
+      /** Assassins */
+      if (message.content === '!demoniste') {  
+            message.channel.send(`https://www.youtube.com/watch?v=XzLU1phP7WY`)};
+      if (message.content === '!blade') {  
+            message.channel.send(`https://www.youtube.com/watch?v=AnAMIuh6wqo`)}; 
+      /** Pas Dispo */
+      if (message.content === '!destructeur') {  
+            message.channel.send(`https://www.youtube.com/watch?v=my1mlgjNXuQ`)};
+      if (message.content === '!lanciere') {  
+            message.channel.send(`https://www.youtube.com/watch?v=wGs6RIcVKMM`)}; 
+      if (message.content === '!scooter') {  
+            message.channel.send(`https://www.youtube.com/watch?v=lmsXXc7WzHs`)}; 
+      if (message.content === '!arcaniste') {  
+            message.channel.send(`https://www.youtube.com/watch?v=6oXB7Cxm5JM`)};
+      if (message.content === '!invocatrice') {  
+            message.channel.send(`https://www.youtube.com/watch?v=yO35t_nvz34`)}; 
+      if (message.content === '!faucheuse') {  
+            message.channel.send(`https://www.youtube.com/watch?v=CWqVheKN3Qw`)}; 
+      if (message.content === '!artiste') {  
+            message.channel.send(`https://www.youtube.com/watch?v=Awa9VkYdWlY`)};  
+    }
